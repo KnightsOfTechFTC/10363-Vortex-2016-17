f6f92ec80db7c7c2aed6b9b062192e6d3c75e2ed
Lego5 <ftcknightsoftech@gmail.com>
2016-09-25 16:18:58 -0500

    Added more Auto Decs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_10363_Auto_Decs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_10363_Auto_Decs.java
@@ -3,10 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 /**
  * Created by Lego5 on 9/25/2016.
@@ -14,6 +18,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class Team_10363_Auto_Decs {
     public DcMotor v_motor_left_drive;
     public DcMotor v_motor_right_drive;
+    GyroSensor SensorGyro;
 
     public void init(HardwareMap ahwMap) {
         try{
@@ -21,6 +26,7 @@ public class Team_10363_Auto_Decs {
             v_motor_left_drive.setDirection(DcMotor.Direction.FORWARD);
             v_motor_left_drive.setPower(0);
             v_motor_left_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            v_motor_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
         catch (Exception p_exception){
             v_motor_left_drive=null;
@@ -31,11 +37,29 @@ public class Team_10363_Auto_Decs {
             v_motor_right_drive.setDirection(DcMotor.Direction.REVERSE);
             v_motor_right_drive.setPower(0);
             v_motor_right_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            v_motor_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
         catch (Exception p_exception){
             v_motor_right_drive=null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
+        try {
+            SensorGyro=ahwMap.gyroSensor.get("gyro");
+        }
+        catch (Exception p_exception){
+            DbgLog.msg(p_exception.getLocalizedMessage());
+            SensorGyro=null;
+        }
+        if (SensorGyro!=null){
+            SensorGyro.calibrate();
+            while(SensorGyro.isCalibrating()){
+                try {
+                    Thread.sleep(50);
+                }
+                catch (Exception p_exception){}
+            }
+        }
+
 
     }
     public void m_left_drive_power(float power){
@@ -63,6 +87,12 @@ public class Team_10363_Auto_Decs {
     }
     public double adjspeed(double speedModifier, int deltaAngle){
         return speedModifier*Math.sin(Math.toRadians(deltaAngle));
+    }
+    public int a_gyro_heading(){
+        if (SensorGyro!=null){
+            return SensorGyro.getHeading();
+        }
+        else {return -36000;}
     }
 
 }
