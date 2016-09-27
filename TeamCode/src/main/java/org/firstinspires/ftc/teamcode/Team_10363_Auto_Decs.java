505eebc603366d2f3627c6edda610ead8eafc15c
Jacob Levine <jacoblevine18@gmail.com>
2016-09-26 22:07:21 -0500

    started to clean up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_10363_Auto_Decs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_10363_Auto_Decs.java
@@ -16,11 +16,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
  * Created by Lego5 on 9/25/2016.
  */
 public class Team_10363_Auto_Decs {
-    public DcMotor v_motor_left_drive;
-    public DcMotor v_motor_right_drive;
-    GyroSensor SensorGyro;
+    //motors
+    private DcMotor v_motor_left_drive;
+    private DcMotor v_motor_right_drive;
+    //sensors
+    private GyroSensor SensorGyro;
 
     public void init(HardwareMap ahwMap) {
+        /*Try to add the left drive motor, stop it if it's moving, set zero power behavior
+        * to "brake", and turn encoders on. */
         try{
             v_motor_left_drive=ahwMap.dcMotor.get("left_drive");
             v_motor_left_drive.setDirection(DcMotor.Direction.FORWARD);
@@ -28,10 +32,12 @@ public class Team_10363_Auto_Decs {
             v_motor_left_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             v_motor_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
+        //If it doesn't work, set the motor to null and add record the problem in the Debug log.
         catch (Exception p_exception){
             v_motor_left_drive=null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
+        //Same as above for the right motor, but reversed
         try {
             v_motor_right_drive=ahwMap.dcMotor.get("right_drive");
             v_motor_right_drive.setDirection(DcMotor.Direction.REVERSE);
@@ -43,6 +49,7 @@ public class Team_10363_Auto_Decs {
             v_motor_right_drive=null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
+        //Try to add the gyro
         try {
             SensorGyro=ahwMap.gyroSensor.get("gyro");
         }
@@ -50,6 +57,7 @@ public class Team_10363_Auto_Decs {
             DbgLog.msg(p_exception.getLocalizedMessage());
             SensorGyro=null;
         }
+        //Calibrate the gyro. Drive team- don't hit start until the light starts blinking!
         if (SensorGyro!=null){
             SensorGyro.calibrate();
             while(SensorGyro.isCalibrating()){
@@ -62,6 +70,7 @@ public class Team_10363_Auto_Decs {
 
 
     }
+    //Modifies the left drive motor's power
     public void m_left_drive_power(float power){
         if (v_motor_left_drive!=null){
             float sendpower= Range.clip(power,-1,1);
@@ -69,25 +78,26 @@ public class Team_10363_Auto_Decs {
         }
 
     }
+    //Same as above, but for the right drive motor
     public void m_right_drive_power(float power){
         if (v_motor_right_drive!=null){
             float sendpower=Range.clip(power,-1,1);
             v_motor_right_drive.setPower(sendpower);
         }
     }
+
+    // Sets both drive motors at once. In teleOp, will have correction if close.
     public void setDrivePower(float left_power, float right_power){
-        if (Math.abs(left_power-right_power)<=.1){
-            m_left_drive_power((left_power+right_power)/2);
-            m_right_drive_power((left_power+right_power)/2);
-        }
-        else {
-            m_left_drive_power(left_power);
-            m_right_drive_power(right_power);
-        }
+        m_left_drive_power(left_power);
+        m_right_drive_power(right_power);
+
     }
+    //calculates adjspeed, the speed correction factor in straightDrive.
     public double adjspeed(double speedModifier, int deltaAngle){
         return speedModifier*Math.sin(Math.toRadians(deltaAngle));
     }
+    /* accesses the gyro's heading. If we can't find it, return an incorrect value that is divisible
+    *  by 360. Remember that the gyro returns a clockwise heading. */
     public int a_gyro_heading(){
         if (SensorGyro!=null){
             return SensorGyro.getHeading();
