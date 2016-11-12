c8eedfbc1e363257864d874961e462174f9a81e0
Other Lego <ftcknightsoftech@gmail.com>
2016-11-11 18:20:39 -0600

    added emergency auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team10363AutoEmergency.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Lego3 on 11/11/2016.
+ */
+@Autonomous(name = "10363 Competition Autonomous on Red Alliance (left beacon)")
+public class Team10363AutoEmergency extends OpMode {
+    Team_10363_Auto_Decs Rob=new Team_10363_Auto_Decs();
+    @Override
+    public void init() {
+        Rob.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        switch (v_state) {
+            case 0:
+                Rob.setDrivePower((float) (.5 - Rob.adjspeed(1, Rob.a_gyro_heading())), (float) (.5 + Rob.adjspeed(1, Rob.a_gyro_heading())));
+                if (Rob.have_drive_encoders_reached(left_encoder - 4640, right_encoder - 4648, false)) {
+                    Rob.setDrivePower(0, 0);
+                    left_encoder = Rob.a_left_encoder_pos();
+                    right_encoder = Rob.a_right_encoder_pos();
+                    v_state++;
+                }
+                break;
+
+            case 1:
+                Rob.setDrivePower((float) .4, 0);
+                if (Rob.a_gyro_heading() >= 260 && Rob.a_gyro_heading() < 290) {
+                    Rob.setDrivePower(0, 0);
+                    left_encoder = Rob.a_left_encoder_pos();
+                    right_encoder = Rob.a_right_encoder_pos();
+                    v_state++;
+                }
+                    break;
+            case 2:
+                Rob.setDrivePower((float) (.2), (float) (.2));
+                if (Rob.have_drive_encoders_reached(left_encoder-6455,right_encoder-6455,false)){
+                    Rob.setDrivePower(0,0);
+                    left_encoder = Rob.a_left_encoder_pos();
+                    right_encoder = Rob.a_right_encoder_pos();
+                    v_state++;
+                }
+                break;
+            default:
+                break;
+
+
+
+        }
+    }
+    int v_state=0;
+    int left_encoder=0;
+    int right_encoder=0;
+}
