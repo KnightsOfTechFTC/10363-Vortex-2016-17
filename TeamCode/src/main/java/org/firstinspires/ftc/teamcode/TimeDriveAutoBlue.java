c4079c2e5dbe84e1e835bb35eef1ae134b3cdfb8
Lego5 <ftcknightsoftech@gmail.com>
2016-11-25 10:19:24 -0600

    started new auto strategy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeDriveAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimeDriveAutoBlue.java
@@ -1,25 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
-        import com.qualcomm.ftccommon.DbgLog;
-        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-        import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.hardware.ColorSensor;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
-        import com.qualcomm.robotcore.hardware.GyroSensor;
-        import com.qualcomm.robotcore.hardware.HardwareMap;
-        import com.qualcomm.robotcore.hardware.I2cAddr;
-        import com.qualcomm.robotcore.hardware.Servo;
-        import com.qualcomm.robotcore.util.ElapsedTime;
-        import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by Lego5 on 11/20/2016.
+ * Created by Lego5 on 11/23/2016.
  */
-@Autonomous(name = "10363 Competition Autonomous on Blue Alliance (right beacon)")
-@Disabled
-public class Team10363AutoBlueLinearRight extends LinearOpMode {
+@Autonomous(name = "timetest")
+public class TimeDriveAutoBlue extends LinearOpMode {
     //Officially, our robot's name is Shartzmugel (votes for: @Lukcio, @Wonnie123, votes against: @jlevine18)
     /* RIP Robert the Robot 2015-2016. May his Res-Q skills be remembered by his 2 children,
         Shartzmugel and JVBot (a temp name) and his lifelong friend 9924Bot.  */
@@ -40,82 +36,80 @@ public class Team10363AutoBlueLinearRight extends LinearOpMode {
     ColorSensor GroundColor;
     ColorSensor LeftColor;
     ColorSensor RightColor;
+
     @Override
+
     public void runOpMode() throws InterruptedException {
 
-        try{
-            v_motor_left_drive= hardwareMap.dcMotor.get("left_drive");
+        try {
+            v_motor_left_drive = hardwareMap.dcMotor.get("left_drive");
             v_motor_left_drive.setDirection(DcMotor.Direction.FORWARD);
             v_motor_left_drive.setPower(0);
             v_motor_left_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             v_motor_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
         //If it doesn't work, set the motor to null and add record the problem in the Debug log.
-        catch (Exception p_exception){
+        catch (Exception p_exception) {
             v_motor_left_drive = null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
         //Same as above for the right motor, but reversed
         try {
-            v_motor_right_drive=hardwareMap.dcMotor.get("right_drive");
+            v_motor_right_drive = hardwareMap.dcMotor.get("right_drive");
             v_motor_right_drive.setDirection(DcMotor.Direction.REVERSE);
             v_motor_right_drive.setPower(0);
             v_motor_right_drive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             v_motor_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-        catch (Exception p_exception){
-            v_motor_right_drive=null;
+        } catch (Exception p_exception) {
+            v_motor_right_drive = null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
-        try{
-            v_motor_intake=hardwareMap.dcMotor.get("intake");
+        try {
+            v_motor_intake = hardwareMap.dcMotor.get("intake");
             v_motor_intake.setDirection(DcMotor.Direction.FORWARD);
             v_motor_intake.setPower(0);
             v_motor_intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             v_motor_intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
         //If it doesn't work, set the motor to null and add record the problem in the Debug log.
-        catch (Exception p_exception){
-            v_motor_intake=null;
+        catch (Exception p_exception) {
+            v_motor_intake = null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
 
         //Try to add the beacon pushing servos. The right one is in reverse.
-        try{
-            v_servo_left_beacon=hardwareMap.servo.get("left beacon");
+        try {
+            v_servo_left_beacon = hardwareMap.servo.get("left beacon");
             v_servo_left_beacon.setDirection(Servo.Direction.FORWARD);
             v_servo_left_beacon.setPosition(.3);
 
-        }
-        catch (Exception p_exception){
-            v_servo_left_beacon=null;
+        } catch (Exception p_exception) {
+            v_servo_left_beacon = null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
-        try{
-            v_servo_right_beacon=hardwareMap.servo.get("right beacon");
+        try {
+            v_servo_right_beacon = hardwareMap.servo.get("right beacon");
             v_servo_right_beacon.setDirection(Servo.Direction.REVERSE);
             v_servo_right_beacon.setPosition(.3);
-        }
-        catch (Exception p_exception){
-            v_servo_right_beacon=null;
+        } catch (Exception p_exception) {
+            v_servo_right_beacon = null;
             DbgLog.msg(p_exception.getLocalizedMessage());
         }
         //Try to add the gyro
         try {
-            SensorGyro=hardwareMap.gyroSensor.get("gyro");
-        }
-        catch (Exception p_exception){
+            SensorGyro = hardwareMap.gyroSensor.get("gyro");
+        } catch (Exception p_exception) {
             DbgLog.msg(p_exception.getLocalizedMessage());
-            SensorGyro=null;
+            SensorGyro = null;
         }
         //Calibrate the gyro. Drive team- don't hit start until the light starts blinking!
-        if (SensorGyro!=null){
+        if (SensorGyro != null) {
             SensorGyro.calibrate();
-            while(SensorGyro.isCalibrating()){
+            while (SensorGyro.isCalibrating()) {
                 try {
                     Thread.sleep(50);
+                } catch (Exception p_exception) {
                 }
-                catch (Exception p_exception){}
             }
         }
         //Try to add the color sensors
@@ -123,28 +117,25 @@ public class Team10363AutoBlueLinearRight extends LinearOpMode {
             GroundColor = hardwareMap.colorSensor.get("ground");
             GroundColor.setI2cAddress(I2cAddr.create8bit(0x3C));
             GroundColor.enableLed(true);
-        }
-        catch (Exception p_exception){
+        } catch (Exception p_exception) {
             DbgLog.msg(p_exception.getLocalizedMessage());
-            GroundColor=null;
+            GroundColor = null;
         }
-        try{
+        try {
             LeftColor = hardwareMap.colorSensor.get("left-color");
             LeftColor.setI2cAddress(I2cAddr.create8bit(0x42));
             LeftColor.enableLed(false);
-        }
-        catch (Exception p_exception){
+        } catch (Exception p_exception) {
             DbgLog.msg(p_exception.getLocalizedMessage());
-            LeftColor=null;
+            LeftColor = null;
         }
         try {
             RightColor = hardwareMap.colorSensor.get("right-color");
             RightColor.setI2cAddress(I2cAddr.create8bit(0x44));
             RightColor.enableLed(false);
-        }
-        catch (Exception p_exception){
+        } catch (Exception p_exception) {
             DbgLog.msg(p_exception.getLocalizedMessage());
-            RightColor=null;
+            RightColor = null;
         }
         left_encoder = a_left_encoder_pos();
         right_encoder = a_right_encoder_pos();
@@ -155,21 +146,99 @@ public class Team10363AutoBlueLinearRight extends LinearOpMode {
             telemetry.update();
             idle();
         }
-        encoderDrive(.5,-10,-10,1.3,-2);
-        reset_drive_encoders();
-        v_motor_left_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         v_motor_right_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        v_motor_left_drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        timedrive(100,.5f,.5f,0);
+        timedrive(500,.5f, -.5f, 45);
         runtime.reset();
-        while ((!(a_gyro_heading()<180&&a_gyro_heading()>45))&&opModeIsActive()&&runtime.seconds()<2){
-            setDrivePower(.5f,-.5f);
-            telemetry.addData("4: Heading", a_gyro_heading());
+        while (a_ground_alpha()<7&&opModeIsActive()&&runtime.seconds()<2.5){
+            telemetry.addData("-1: time driving",runtime.milliseconds());
+            double adjspeed=(.5+.5)*Math.sin(((2*Math.PI)/360)*(a_gyro_heading()-45));
+            telemetry.addData("2: adjspeed: " ,adjspeed);
+            v_motor_left_drive.setPower(.5-adjspeed);
+            v_motor_right_drive.setPower(.5+adjspeed);
+            telemetry.addData("5: Heading ", a_gyro_heading());
+            telemetry.addData("6: Ground Color (Blue) ", a_ground_blue());
+            telemetry.addData("7: Ground Color (Alpha) ", a_ground_alpha());
+            telemetry.addData("8: Beacon Red ", a_left_red());
+            telemetry.addData("9: Beacon Blue ", a_left_blue());
+            telemetry.addData("10: last state left ", left_encoder);
+            telemetry.addData("11: last state right ", right_encoder);
+            telemetry.addData("12: actual left power ", actual_left_power());
             telemetry.update();
+
+            // Allow time for other processes to run.
+            idle();
+
         }
-        v_motor_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        v_motor_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        encoderDrive(.5,-40.67,-40.67,3,45);
+        timedrive(500,-.5f, .5f, 0);
 
 
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void timedrive(int mills, float speedleft, float speedright, int targetheading) throws InterruptedException{
+        runtime.reset();
+        while (opModeIsActive() && runtime.milliseconds()<mills) {
+                telemetry.addData("-1: time driving",runtime.milliseconds());
+                if (targetheading>=0&&targetheading<=360){
+                double adjspeed=(speedleft+speedright)*Math.sin(((2*Math.PI)/360)*(a_gyro_heading()-targetheading));
+                telemetry.addData("2: adjspeed: " ,adjspeed);
+                v_motor_left_drive.setPower(speedleft-adjspeed);
+                v_motor_right_drive.setPower(speedright+adjspeed);
+            }
+            else{
+                v_motor_left_drive.setPower(speedleft);
+                v_motor_right_drive.setPower(speedright);
+            }
+
+            telemetry.addData("5: Heading ", a_gyro_heading());
+            telemetry.addData("6: Ground Color (Blue) ", a_ground_blue());
+            telemetry.addData("7: Ground Color (Alpha) ", a_ground_alpha());
+            telemetry.addData("8: Beacon Red ", a_left_red());
+            telemetry.addData("9: Beacon Blue ", a_left_blue());
+            telemetry.addData("10: last state left ", left_encoder);
+            telemetry.addData("11: last state right ", right_encoder);
+            telemetry.addData("12: actual left power ", actual_left_power());
+            telemetry.update();
+
+            // Allow time for other processes to run.
+            idle();
+        }
     }
 
     //methods
@@ -319,71 +388,5 @@ public class Team10363AutoBlueLinearRight extends LinearOpMode {
         }
         return returnthis;
     }
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS, int angleTarget) throws InterruptedException {
-        int newLeftTarget;
-        int newRightTarget;
-        double COUNTS_PER_INCH=2240/(3.54331*Math.PI);
-
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
-
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = v_motor_left_drive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = v_motor_right_drive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            v_motor_left_drive.setTargetPosition(newLeftTarget);
-            v_motor_right_drive.setTargetPosition(newRightTarget);
-
-            // Turn On RUN_TO_POSITION
-            v_motor_left_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            v_motor_right_drive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            // reset the timeout time and start motion.
-            runtime.reset();
-            v_motor_left_drive.setPower(Math.abs(speed));
-            v_motor_right_drive.setPower(Math.abs(speed));
-
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (v_motor_left_drive.isBusy() && v_motor_right_drive.isBusy())) {
-                telemetry.addData("-1: time driving",runtime.milliseconds());
-                telemetry.addData("0: left target",newLeftTarget);
-                telemetry.addData("1: right target",newRightTarget);
-                if (angleTarget>=0&&angleTarget<=360){
-                    double adjspeed=speed*Math.sin(((2*Math.PI)/360)*(a_gyro_heading()-angleTarget));
-                    v_motor_left_drive.setPower(Math.abs(speed)-adjspeed);
-                    v_motor_right_drive.setPower(Math.abs(speed)+adjspeed);
-                    telemetry.addData("2: adjspeed: " ,adjspeed);
-                }
-
-                telemetry.addData("3: left encoder ", a_left_encoder_pos());
-                telemetry.addData("4: right encoder ", a_right_encoder_pos());
-                telemetry.addData("5: Heading ", a_gyro_heading());
-                telemetry.addData("6: Ground Color (Blue) ", a_ground_blue());
-                telemetry.addData("7: Ground Color (Alpha) ", a_ground_alpha());
-                telemetry.addData("8: Beacon Red ", a_left_red());
-                telemetry.addData("9: Beacon Blue ", a_left_blue());
-                telemetry.addData("10: last state left ", left_encoder);
-                telemetry.addData("11: last state right ", right_encoder);
-                telemetry.addData("12: actual left power ", actual_left_power());
-                telemetry.update();
-
-                // Allow time for other processes to run.
-                idle();
-            }
-
-            // Stop all motion;
-            v_motor_left_drive.setPower(0);
-            v_motor_right_drive.setPower(0);
-
-            // Turn off RUN_TO_POSITION
-            v_motor_left_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            v_motor_right_drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-            //  sleep(250);   // optional pause after each move
-        }
-    }
-
 }
+
